633eecae72c1c7c66c62bc004684843441ea72db
Marcelo Aguero <marceloaguero@gmail.com>
2020-03-22 16:17:44 -0300

    Delete blank lines

--- arrays/sum_test.go
+++ arrays/sum_test.go
@@ -38,14 +38,12 @@ func TestSumAllTails(t *testing.T) {
 	t.Run("make the sums of some slices", func(t *testing.T) {
 		got := SumAllTails([]int{1, 2}, []int{0, 9})
 		want := []int{2, 9}
-
 		checkSum(t, got, want)
 	})
 
 	t.Run("safely sum empty slices", func(t *testing.T) {
 		got := SumAllTails([]int{}, []int{3, 4, 5})
 		want := []int{0, 9}
-
 		checkSum(t, got, want)
 	})
 }
